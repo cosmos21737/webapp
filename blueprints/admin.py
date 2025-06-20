c1bdf93a680673e0334810c2e7050649e29befe8
cosmos21737 <cosmos21737@gmail.com>
2025-06-20 14:43:56 +0900

    測定項目の削除を実装。

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -4,15 +4,19 @@ from flask_security import roles_required, roles_accepted
 from sqlalchemy import func
 
 from services import services
-from db_models import db, User, MeasurementRecord
+from db_models import db, User, MeasurementRecord, MeasurementType
 
 admin_bp = Blueprint('admin', __name__)
+
 
 @admin_bp.route('/admin')
 @login_required
 @roles_required("administer")
 def admin():
-    return render_template('admin.html')
+    measurement_types = MeasurementType.query.all()
+    return render_template('admin.html',
+                           measurement_types=measurement_types
+                           )
 
 
 @admin_bp.route('/data_delete')
@@ -32,3 +36,24 @@ def data_delete():
         flash(f'初期化中にエラーが発生しました: {e}', 'danger')
 
     return redirect(url_for('main.dashboard'))
+
+
+@admin_bp.route('/admin/delete_measurement_type/<int:type_id>', methods=['POST'])
+@login_required
+@roles_required("administer")
+def delete_measurement_type(type_id):
+    measurement_type = MeasurementType.query.get(type_id)
+
+    if not measurement_type:
+        flash('指定された測定項目が見つかりませんでした。', 'danger')
+        return redirect(url_for('admin.admin'))
+
+    try:
+        db.session.delete(measurement_type)
+        db.session.commit()
+        flash(f'測定項目「{measurement_type.display_name}」を削除しました。', 'success')
+    except Exception as e:  # IntegrityErrorを特別に処理する必要がなくなる
+        db.session.rollback()
+        flash(f'測定項目の削除中にエラーが発生しました: {e}', 'danger')
+
+    return redirect(url_for('admin.admin'))
